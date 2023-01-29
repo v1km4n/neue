fbe0ff09516344bf92e52ca18dacf60d8ee118dc
Alex Pal <v1km4n@ya.ru>
2023-01-29 17:53:30 +0300

    reworked etf2l.js

--- commands/etf2l.js
+++ commands/etf2l.js
@@ -9,222 +9,127 @@ module.exports = {
 	name: 'etf2l',
 	description: 'ETF2L League Checker',
 	execute(message, args) {
-        var request = new XMLHttpRequest();
-		var ETF2LLink;
-
 		steam.resolve(args[0]).then(steamID => {
-			//ETF2L
-
-			let etf2lPlayerURL = new URL("https://api.etf2l.org/player/") + steamID + (".json");
-			request.open('GET', etf2lPlayerURL, false);
+			var request = new XMLHttpRequest();
+			request.open('GET', new URL(`https://api.etf2l.org/player/${steamID}.json`), false);
 			request.send();
 
 			var etf2lPlayer = JSON.parse(request.responseText);
-
-			if (etf2lPlayer.status.code == 200) {
-				let ETF2LID = etf2lPlayer.player.id;
-				let ETF2LNickName = etf2lPlayer.player.name;
-				let ETF2lCountry = etf2lPlayer.player.country;
-				ETF2LLink = "https://etf2l.org/forum/user/" + ETF2LID;
-				let ETF2LProfilePicture = etf2lPlayer.player.steam.avatar;
-
-				var HLTeamNo = null;
-				var SixesTeamNo = null;
-
-				var HLTeamLink = null;
-				var HLTeamDiv = null;
-				var HLTeamName = null;
-				var HLTeamCompetititonName = null;
-
-				var SixesTeamLink = null;
-				var SixesTeamDiv = null;
-				var SixesTeamName = null;
-				var SixesTeamCompetititonName = null;
-
-
-				if (etf2lPlayer.player.teams != null) {
-					for (let i = 0; i < Object.keys(etf2lPlayer.player.teams).length; i++) {
-						if (etf2lPlayer.player.teams[i].type == "Highlander") { 
+			if (etf2lPlayer.status.code == 200) 
+			{
+				var HLTeamNo;
+				var sixesTeamNo;
+				if (etf2lPlayer.player.teams != null) 
+				{
+					for (let i = 0; i < Object.keys(etf2lPlayer.player.teams).length; i++) 
+					{
+						if (etf2lPlayer.player.teams[i].type == "Highlander") 
+						{ 
 							HLTeamNo = i;
-						} else if (etf2lPlayer.player.teams[i].type == "6on6") {
-							SixesTeamNo = i;
+						} 
+						else if (etf2lPlayer.player.teams[i].type == "6on6") 
+						{
+							sixesTeamNo = i;
 						} 
 					}
 				}
 
-				if (HLTeamNo != null) {
-					let latestSeasonID = null;
-					let checkedSeason = null;
-
-					let team = etf2lPlayer.player.teams[HLTeamNo];
-					let listofHLTeamSeasons = etf2lPlayer.player.teams[HLTeamNo].competitions;
-					if (listofHLTeamSeasons !== null) { //team exists, but haven't played any matches yet (HLTeamDiv stays null)
-						let amountofHLTeamSeasons = Object.keys(etf2lPlayer.player.teams[HLTeamNo].competitions).length;
-						let latestSeason = null; //meaning the JSON part of the season we are about to get info about
-
-						HLTeamLink = "https://etf2l.org/teams/" + team.id;
-						HLTeamName = team.name;
-
-						for (let i = 0; i < amountofHLTeamSeasons; i++) {
-							checkedSeason = Object.keys(listofHLTeamSeasons)[amountofHLTeamSeasons - i - 1];
-							if ((!listofHLTeamSeasons[checkedSeason].competition.includes("Qualifiers")) && 
-								(!listofHLTeamSeasons[checkedSeason].competition.includes("Playoffs"))) {
-								latestSeasonID = checkedSeason;
-								latestSeason = etf2lPlayer.player.teams[HLTeamNo].competitions[latestSeasonID];
-								break; //found the latest HL season that is not qualifiers of playoffs, since those don't have the division argument
-							}
-						}
-
-						if (latestSeasonID == null) {
-							HLTeamCompetititonName = null;
-							HLTeamDiv = null;
-						} else { //TODO: add check for the player match history rather than division of the current team
-							if ((latestSeason.division.name == null)) { //for those HL seasons, where open division was basically a complete different season
-								let colonIndex = latestSeason.competition.indexOf(':'); //we need this to find colon in string like "Highlander Season 18: Open" and the slice the ": Open" part off
-								HLTeamCompetititonName = latestSeason.competition.slice(0, colonIndex); //"Highlander Season 18: Open" -> "Highlander Season 18"
-								HLTeamDiv = "Open";
-							} else { //now for the usual seasons
-								HLTeamDiv = latestSeason.division.name;
-								HLTeamCompetititonName = latestSeason.competition;
-							}
-						}
-					}
-					
-				} else {
-					HLTeamLink = null;
+				var HLTeam;
+				if (HLTeamNo != undefined)
+				{
+					HLTeam = gameModeParse(etf2lPlayer, HLTeamNo);
 				}
 
-				if (SixesTeamNo != null) {
-					let latestSeasonID = null;
-					let checkedSeason = null;
-
-					let team = etf2lPlayer.player.teams[SixesTeamNo];
-					let listofSixesTeamSeasons = etf2lPlayer.player.teams[SixesTeamNo].competitions;
-					if (listofSixesTeamSeasons !== null) { //team exists, but haven't played any matches yet (SixesTeamDiv stays null)
-						let amountofSixesTeamSeasons = Object.keys(etf2lPlayer.player.teams[SixesTeamNo].competitions).length;
-						let latestSeason = null;
-
-						SixesTeamLink = "https://etf2l.org/teams/" + team.id;
-						SixesTeamName = team.name;
-
-						for (let i = 0; i < amountofSixesTeamSeasons; i++) {
-							checkedSeason = Object.keys(listofSixesTeamSeasons)[amountofSixesTeamSeasons - i - 1];
-							if ((!listofSixesTeamSeasons[checkedSeason].competition.includes("Qualifiers")) && 
-								(!listofSixesTeamSeasons[checkedSeason].competition.includes("Playoffs"))) {
-								latestSeasonID = checkedSeason;
-								latestSeason = etf2lPlayer.player.teams[SixesTeamNo].competitions[latestSeasonID];
-								break;
-							}
-						}
-
-						if (latestSeasonID == null) {
-							SixesTeamCompetititonName = null;
-							SixesTeamDiv = null;
-						} else { //TODO: add check for the player match history rather than division of the current team
-							if ((latestSeason.division.name == null)) { //for those HL seasons, where open division was basically a complete different season
-								let colonIndex = latestSeason.competition.indexOf(':'); //we need this to find colon in string like "Highlander Season 18: Open" and the slice the ": Open" part off
-								SixesTeamCompetititonName = latestSeason.competition.slice(0, colonIndex); //"Highlander Season 18: Open" -> "Highlander Season 18"
-								SixesTeamDiv = "Open";
-							} else { //now for the usual seasons
-								SixesTeamDiv = latestSeason.division.name;
-								SixesTeamCompetititonName = latestSeason.competition;
-							}
-						}
-					}						
-					
-					
-				} else {
-					SixesTeamLink = null;
+				if (HLTeamNo == undefined) //no team
+				{
+					var HLEmbedDescription = "None";
+				} 
+				else if (HLTeam.seasonDiv == null) //team with no seasons played
+				{
+					var HLEmbedDescription = `${HLTeam.name} | ${HLTeam.link}`
+				} 
+				else if (HLTeam.seasonDiv != null) //team with seasons played
+				{
+					var HLEmbedDescription = `${HLTeam.name} | ${HLTeam.link}\n${HLTeam.seasonName} | ${HLTeam.seasonDiv}`;
 				}
 
-				// ETF2L Info for the ${ETF2LNickName}
-				// 
-				// Profile Link
-				// ${ETF2LLink}
-				//
-				// 6v6 Team		
-				// ${SixesTeamName} | ${SixesTeamLink}											
-				// ${SixesTeamCompetititonName} | ${SixesTeamDiv}
-				// 
-				// HL Team
-				// ${HLTeamName} | ${HLTeamLink}
-				// ${HLTeamCompetititonName} | ${HLTeamDiv}
-
-				let SixesEmbedDescription;
-				if (SixesTeamLink == null) {
-					SixesEmbedDescription = "None";
-				} else if (SixesTeamDiv == null) {
-					SixesEmbedDescription = `${SixesTeamName} | ${SixesTeamLink}`
-				} else if (SixesTeamDiv != null) {
-					SixesEmbedDescription = `${SixesTeamName} | ${SixesTeamLink}\n${SixesTeamCompetititonName} | ${SixesTeamDiv}`;
-				} else {
-					SixesEmbedDescription = `Unknown error has occured *shrug*`;
+				var sixesTeam;
+				if (sixesTeamNo != undefined)
+				{
+					sixesTeam = gameModeParse(etf2lPlayer, sixesTeamNo);
 				}
 
-				let HLEmbedDescription;
-				if (HLTeamLink == null) {
-					HLEmbedDescription = "None";
-				} else if (HLTeamDiv == null) {
-					HLEmbedDescription = `${HLTeamName} | ${HLTeamLink}`
-				} else if (HLTeamDiv != null) {
-					HLEmbedDescription = `${HLTeamName} | ${HLTeamLink}\n${HLTeamCompetititonName} | ${HLTeamDiv}`;
-				} else {
-					HLEmbedDescription = `Unknown error has occured *shrug*`;
+				if (sixesTeamNo == undefined) //no team
+				{
+					var sixesEmbedDescription = "None";
+				} 
+				else if (sixesTeam.seasonDiv == null) //team with no seasons played
+				{
+					var sixesEmbedDescription = `${sixesTeam.name} | ${sixesTeam.link}`
+				} 
+				else if (sixesTeam.seasonDiv != null) //team with seasons played
+				{
+					var sixesEmbedDescription = `${sixesTeam.name} | ${sixesTeam.link}\n${sixesTeam.seasonName} | ${sixesTeam.seasonDiv}`;
 				}
-				
+								
 				var ETF2LEmbed = new Discord.MessageEmbed()
 
 					.setColor('#0099ff')
-					.setTitle(`ETF2L: ${ETF2LNickName} (${ETF2lCountry})`)
-					.setDescription(`${ETF2LLink}`)
-					.setThumbnail(ETF2LProfilePicture)
+					.setTitle(`ETF2L: ${etf2lPlayer.player.name} (${etf2lPlayer.player.country})`)
+					.setDescription(`https://etf2l.org/forum/user/${etf2lPlayer.player.id}`)
+					.setThumbnail(etf2lPlayer.player.steam.avatar)
 					.addFields(
-						{ name: '**6v6**', value: SixesEmbedDescription},
+						{ name: '**6v6**', value: sixesEmbedDescription},
 						{ name: '**Highlander**', value: HLEmbedDescription},
 					)
 				message.channel.send(ETF2LEmbed);
 			} else {
-				message.channel.send("No such player on ETF2L");
+				message.channel.send("This Steam account is not associated with a ETF2L profile");
 			}
-			
 		});
-	
-    	},
+    },
 };
 
-function gameModeParse (gameMode, player) {
-	let latestSeasonID = null;
+
+//using teamN instead of teamID since it is not an etf2l id of a team but rather a number of a team in the player team history
+function gameModeParse (etf2lPlayer, teamN) 
+{
+	let team = etf2lPlayer.player.teams[teamN];
+	let teamSeasons = etf2lPlayer.player.teams[teamN].competitions;
+	let teamSeasonsN = Object.keys(etf2lPlayer.player.teams[teamN].competitions).length;
+	let latestSeason = null;
+
+	//unfortunately we cannot be sure that if a team has any seasons played then we have a season to parse, since what we wanna parse should not be playoffs or a preseason
+	//and they both count as a separate seasons in the team history
+
 	let checkedSeason = null;
-
-	let team = etf2lPlayer.player.teams[HLTeamNo];
-	let listofHLTeamSeasons = etf2lPlayer.player.teams[HLTeamNo].competitions;
-	let amountofHLTeamSeasons = Object.keys(etf2lPlayer.player.teams[HLTeamNo].competitions).length;
-	let latestSeason = null; //meaning the JSON part of the season we are about to get info about
-
-	HLTeamLink = "https://etf2l.org/teams/" + team.id;
-	HLTeamName = team.name;
-
-	for (let i = 0; i < amountofHLTeamSeasons; i++) {
-		checkedSeason = Object.keys(listofHLTeamSeasons)[amountofHLTeamSeasons - i - 1];
-		if ((!listofHLTeamSeasons[checkedSeason].competition.includes("Qualifiers")) && 
-			(!listofHLTeamSeasons[checkedSeason].competition.includes("Playoffs"))) {
-			latestSeasonID = checkedSeason;
-			latestSeason = etf2lPlayer.player.teams[HLTeamNo].competitions[latestSeasonID];
-			break; //found the latest HL season that is not qualifiers of playoffs, since those don't have the division argument
+	for (let i = 0; i < teamSeasonsN; i++) 
+	{
+		checkedSeason = Object.keys(teamSeasons)[teamSeasonsN - i - 1];
+		if ((!teamSeasons[checkedSeason].competition.includes("Qualifiers")) && (!teamSeasons[checkedSeason].competition.includes("Playoffs"))) 
+		{
+			latestSeason = etf2lPlayer.player.teams[teamN].competitions[checkedSeason];
+			break; 
+			//found the latest season that is not qualifiers of playoffs
 		}
 	}
 
-	if (latestSeasonID == null) {
-		HLTeamCompetititonName = null;
-		HLTeamDiv = null;
-	} else { //TODO: add check for the player match history rather than division of the current team
-		if ((latestSeason.division.name == null)) { //for those HL seasons, where open division was basically a complete different season
-			let colonIndex = latestSeason.competition.indexOf(':'); //we need this to find colon in string like "Highlander Season 18: Open" and the slice the ": Open" part off
-			HLTeamCompetititonName = latestSeason.competition.slice(0, colonIndex); //"Highlander Season 18: Open" -> "Highlander Season 18"
-			HLTeamDiv = "Open";
-		} else { //now for the usual seasons
-			HLTeamDiv = latestSeason.division.name;
-			HLTeamCompetititonName = latestSeason.competition;
-		}
+	var result = new Object();
+	//result represents a team with name, link, and the latest played season's div and name
+
+	result.name = team.name;
+	result.link = `https://etf2l.org/teams/${team.id}`;
+
+	if (latestSeason == null) // team has not participated in any seasons
+	{
+		result.seasonName = null;
+		result.seasonDiv = null;
+	} 
+	else 
+	{
+		result.seasonName = latestSeason.competition;
+		result.seasonDiv = latestSeason.division.name;
 	}
+
+	return result;
 }
